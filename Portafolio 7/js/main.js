e0f5f7900541c5fcb6f46d6515555c1f83e1e774
Gerson Flores Narciso <110065310+Gerson12-afk@users.noreply.github.com>
2022-12-03 19:47:37 -0500

    Portafolio

---
+++ Portafolio 7/js/main.js
@@ -0,0 +1,14 @@
+document.getElementById("btn").addEventListener("click" ,mostrar_menu)
+document.getElementById("back-Menu").addEventListener("click" ,ocultar_menu)
+const contenido = document.getElementById("Navegador");
+const colorMenu = document.getElementById("back-Menu")
+
+function mostrar_menu(){
+    contenido.style.right = "0px";
+    colorMenu.style.display="block"
+}
+
+function ocultar_menu(){
+    contenido.style.right = "-350px";
+    colorMenu.style.display="none"
+}
